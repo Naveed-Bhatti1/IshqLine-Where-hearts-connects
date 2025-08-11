681ffa3fcc4b6dd681e2bd38c7e078afcd728907
kairen-dev <codewithkairen@gmail.com>
2025-08-11 16:41:19 +0500

    User Schema Added

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    otp: { type: String },
+    otpExpires: { type: Date },
+    isVerified: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
